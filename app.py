52c4c186553dcbb93a07d42a6dc65e74629fa73f
maulanaulia <maulana-air@maulanas-MacBook-Air.local>
2017-06-13 23:23:51 +0800

    add tempo func

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+import time
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'devkey'
 
 @app.route('/hello')
 def hello():
@@ -8,10 +10,22 @@ def hello():
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if 'timelist' not in session:
+        session['timelist'] = 0
+    
     if request.method == 'POST':
-        return render_template('index.html', curTempo= '1')
+        if 'last_time' not in session:
+            session['last_time'] = time.time()
+            return render_template('index.html', curTempo = '--')
 
-    return render_template('index.html', curTempo = '0')
+        session['last_time'] = session['timelist']
+        session['timelist'] = time.time()
+        #tempo = str(session['timelist']) + ' ' + str(session['last_time'])
+        tempo = 1 * 60 / (session['timelist'] - session['last_time'])
+
+        return render_template('index.html', curTempo= format(tempo, '.0f'))
+
+    return render_template('index.html', curTempo = '--')
 
 if __name__ == '__main__':
     app.run()
