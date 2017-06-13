cbd918b4ec713dc5ffbc98e2add0d9874129c1fb
maulanaulia <maulana-air@maulanas-MacBook-Air.local>
2017-06-13 14:24:45 +0800

    reroute and templates

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/hello')
+def hello():
+    return 'Hello stinkin world!'
+
 @app.route('/')
 def index():
-    return 'Hello stinkin world!'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
