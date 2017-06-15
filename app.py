9e3b6a388b5fe58f15f8fabbdd5281f8a83ab689
maulanaulia <maulana-air@maulanas-MacBook-Air.local>
2017-06-15 12:54:45 +0800

    refactor tempo algo
    Signed-off-by: maulanaulia <maulana-air@maulanas-MacBook-Air.local>

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, session
 import time
+import functools
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'devkey'
+
+def add_delta(x, y):
+    return float(x) + float(y)
 
 @app.route('/hello')
 def hello():
@@ -11,19 +15,29 @@ def hello():
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if 'timelist' not in session:
-        session['timelist'] = 0
+        session['timelist'] = []
     
     if request.method == 'POST':
         if 'last_time' not in session:
             session['last_time'] = time.time()
             return render_template('index.html', curTempo = '--')
 
-        session['last_time'] = session['timelist']
-        session['timelist'] = time.time()
-        #tempo = str(session['timelist']) + ' ' + str(session['last_time'])
-        tempo = 1 * 60 / (session['timelist'] - session['last_time'])
+        curr_time = time.time()
+        t_delta = curr_time - session['last_time']
+        session['last_time'] = curr_time
 
-        return render_template('index.html', curTempo= format(tempo, '.0f'))
+        if len(session['timelist']) > 3:
+            session['timelist'].pop(0)
+
+        #session['timelist'].append(format(t_delta, '.2f'))
+        session['timelist'].append(t_delta)
+        session.modified = True
+
+        avg_delta = functools.reduce(add_delta, session['timelist'])
+        tempo = 1 * 60 / (avg_delta / len(session['timelist']))
+
+        #return render_template('index.html', curTempo= format(tempo, '.0f'))
+        return render_template('index.html', curTempo= str(tempo))
 
     return render_template('index.html', curTempo = '--')
 
